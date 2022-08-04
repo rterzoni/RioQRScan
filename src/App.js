23e44eee9b900a958b72b4378a9aa96a81727ed0
rterzoni <rafael.terzoni@gmail.com>
2022-08-04 16:25:28 +1000

    link_undone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React from 'react';
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
+function App() {
+  const value = 'World';
+  return <div>Hello {value}</div>;
+}
 
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
-});
-
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
-});
-
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
+export default App;
